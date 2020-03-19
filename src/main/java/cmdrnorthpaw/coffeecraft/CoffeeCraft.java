e7107219ac30f6e4860cfee50251a46554198eca
CmdrNorthpaw <thetuxkid@gmail.com>
2020-03-19 16:48:02 +0000

    Main mod class is here!

---
+++ src/main/java/cmdrnorthpaw/coffeecraft/CoffeeCraft.java
@@ -0,0 +1,30 @@
+package cmdrnorthpaw.coffeecraft;
+
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("coffeecraft")
+public class CoffeeCraft {
+    public static final String MODID = "coffeecraft";
+
+    public static IProxy proxy = DistExecutor.runForDist(() -> () -> new ClientProxy(), () -> () -> new ServerProxy());
+
+    public static ModSetup setup = new ModSetup();
+
+    private static final Logger logger = LogManager.getLogger();
+
+    public CoffeeCraft() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        setup.init();
+        proxy.init();
+    }
+
+}
